2321b889a9f2222d1e1a6bfcab03063b5e151bb7
Jean Candido <146030723+JeanCandido@users.noreply.github.com>
2025-03-14 16:26:36 -0300

    a

--- script.py
+++ script.py
@@ -1,6 +1,17 @@
-i = 1
-n = int(input("Digite se numero para saber a tabuada: "))
+while True:
+    i = 1
+    n = int(input("Digite se numero para saber a tabuada: "))
 
-while i <= 10:
-    print(f"{i} x {n} = {i * n}")
-    i += 1
+    while i <= 10:
+        print(f"{i} x {n} = {i * n}")
+        i += 1
+    
+    sair = input("voce quer sair? (s/n):")
+    
+    if sair == 's':
+        print("Bye!")
+        break
+    elif sair == 'n':
+        print('pode continuar')
+    else:
+        print("digite uma resposta valida")
---
+++ teste.py
@@ -0,0 +1,5 @@
+dia = int(input("digite o dia do seu anivesario:"))
+mes = int(input("digite o mes do seu anivesario:"))
+ano = int(input("digite o ano do seu nascimento:"))
+
+print(f'Você nasceu nesta data: {dia}/{mes}/{ano}')
