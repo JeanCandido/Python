1027442418032fb387bb9a43c3d567982d2c7059
Jean Candido <146030723+JeanCandido@users.noreply.github.com>
2025-03-14 16:45:25 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,4 +2,11 @@ dia = int(input("digite o dia do seu anivesario:"))
 mes = int(input("digite o mes do seu anivesario:"))
 ano = int(input("digite o ano do seu nascimento:"))
 
+if ano <= 26:
+    ano += 2000
+elif ano > 100:
+    ano = ano
+else:
+    ano += 1900
+
 print(f'Você nasceu nesta data: {dia}/{mes}/{ano}')
